9418a9a94017cfcf7a44cf7542199548c980759e
Walter Shewmake <25shewmwalj@washk12.org>
2021-04-17 17:19:26 -0600

    asd

--- start.js
+++ start.js
@@ -2,6 +2,8 @@ const {
     exec
 } = require('child_process');
 
+console.log("THE PORT IS: " + process.env.PORT);
+
 exec('json-server --watch db.json -p ' + process.env.PORT, (error, stdout, stderr) => {
     if (error) {
         return console.log(`error: ${error.message}`);
